7be16ba0ac02f96c2e9a32ba27760e8f4d8bd873
Stefania-Larisa Barabas <stefaniabarabas@gmail.com>
2024-11-20 14:50:49 +0200

    Theming and fonts

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,45 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { auth, signOut, signIn } from "@/auth";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
   return (
     <header className="px-2 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
           <Image src="/logo.png" alt="Pitch App Logo" width={144} height={30} />
         </Link>
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut();
+                }}
+              >
+                <button type="submit">Logout</button>
+              </form>
+              <Link href={`/user/${session?.user?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit"> Login </button>
+            </form>
+          )}
+        </div>
       </nav>
     </header>
   );
