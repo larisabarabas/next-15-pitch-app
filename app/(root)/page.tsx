d5cc5b4dcf0ae24165f11e73c22a728735ac2e87
Stefania-Larisa Barabas <stefaniabarabas@gmail.com>
2024-11-27 15:00:15 +0200

    Implementing Sanity for CMS

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -6,16 +7,49 @@ export default async function Home({
   readonly searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: {
+        _id: 1,
+        name: "Stefania",
+      },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://images.unsplash.com/photo-1527430253228-e93688616381?q=80&w=1334&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
   return (
-    <section className="pink_container">
-      <h1 className="heading">
-        Pitch your startup, <br /> Connect with entrepreneurs
-      </h1>
-      <p className="sub-heading !max-w-3xl">
-        {" "}
-        Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions
-      </p>
-      <SearchForm query={query} />
-    </section>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your startup, <br /> Connect with entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          {" "}
+          Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results"> No startups found</p>
+          )}
+        </ul>
+      </section>
+    </>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    // TO DO: To be removed later
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
