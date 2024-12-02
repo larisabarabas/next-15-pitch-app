490cd9e9a43b04bdec7424c605b2ac6ff19e5b01
Stefania-Larisa Barabas <stefaniabarabas@gmail.com>
2024-12-02 15:45:40 +0200

    Saving author to Sanity once the user logs in with Github

--- auth.ts
+++ auth.ts
@@ -1,6 +1,54 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
+import { client } from "@/sanity/lib/client";
+import { writeClient } from "@/sanity/lib/write-client";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
+  callbacks: {
+    async signIn({
+      user: { name, email, image },
+      profile: { id, login, bio },
+    }) {
+      const existingUser = await client
+        .withConfig({ useCdn: false })
+        .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+          id,
+        });
+
+      if (!existingUser) {
+        await writeClient.create({
+          _type: "author",
+          id,
+          name,
+          username: login,
+          email,
+          image,
+          bio: bio || "",
+        });
+      }
+
+      return true;
+    },
+    async jwt({ token, account, profile }) {
+      if (account && profile) {
+        console.log("PROFILE:", profile);
+        const user = await client
+          .withConfig({ useCdn: false })
+          .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+            id: profile?.id,
+          });
+        console.log("USER:", user);
+        token.id = profile?.id;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      console.log("TOKEN:", token);
+      Object.assign(session, { id: token.id });
+      return session;
+    },
+  },
 });
