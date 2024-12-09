41bb8e0782b9a2443afcbce9922066889f7e6a04
Stefania-Larisa Barabas <stefaniabarabas@gmail.com>
2024-12-09 14:22:05 +0200

    Ignoring ts build errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     // TO DO: To be removed later
     dangerouslyAllowSVG: true,
